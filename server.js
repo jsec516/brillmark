c63979cb3f54f021e0002d1ff482df09897cc785
MD. Jahidul Islam <jsec516@gmail.com>
2017-08-27 14:22:38 +0600

    updated

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var nodemailer = require("nodemailer");
 var bodyParser = require('body-parser');
 var cors = require('cors');
 const sendmail = require('sendmail')();
+var helper = require('sendgrid').mail;
 var app=express();
 try {
   const envStat = require('fs').statSync('.env');
@@ -17,7 +18,7 @@ catch (e) {
   console.error('Failed to load env file', e);
 }
 
-
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 var corsOptions = {
   origin: 'http://brillmark.s3-website-us-west-1.amazonaws.com',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
@@ -29,21 +30,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
     Here we are configuring our SMTP Server details.
     STMP is mail server which is responsible for sending and recieving email.
 */
-var smtpTransport = nodemailer.createTransport({
+/*var smtpTransport = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
     auth: {
         user: process.env.email_user,
         pass: process.env.email_password
     }
-});
+});*/
 /*------------------SMTP Over-----------------------------*/
 
 /*------------------Routing Started ------------------------*/
 
 
+app.get('/test', function(req, res) {
+	
+var fromEmail = new helper.Email('jahidul@brillmark.com');
+var toEmail = new helper.Email('info@brillmark.com');
+var subject = 'Contact Support';
+var content = new helper.Content('text/plain', 'awesome message');
+var mail = new helper.Mail(fromEmail, subject, toEmail, content);
+ 
+
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON()
+});
+ 
+sg.API(request, function (error, response) {
+  if (error) {
+    console.log('Error response received');
+    return res.send(error);
+  }
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+  return res.send(response);
+});
+});
 app.post('/send', cors(corsOptions), function(req,res){
-	sendmail({
+	/*sendmail({
 	    from: req.body.email,
 	    to: 'jahidul@brillmark.com, info@brillmark.com',
 	    subject: 'contact',
@@ -60,7 +87,8 @@ app.post('/send', cors(corsOptions), function(req,res){
 		}
 	    
 	    
-	});
+	});*/
+	
     
 });
 
